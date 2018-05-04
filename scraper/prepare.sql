b5889d2698e3f94694a857708b3167c60bdbb181
Evan James <ejames917@gmail.com>
2018-05-04 15:01:24 -0600

    Full json files for 500 links

--- scraper/prepare.sql
+++ scraper/prepare.sql
@@ -5,6 +5,7 @@
 ALTER TABLE Songs ADD ArtistName char(128);
 UPDATE Songs A SET ArtistName = (SELECT Name FROM Artists B WHERE A.Artist = B.ID);
 
+/*
 CREATE OR REPLACE VIEW SampleExportRel AS SELECT * FROM Rel LIMIT 25;
 
 CREATE OR REPLACE VIEW SampleExportDillaSongs AS
@@ -14,12 +15,13 @@ CREATE OR REPLACE VIEW SampleExportDillaSongs AS
 CREATE OR REPLACE VIEW SampleExportSampledSongs AS
   SELECT * FROM Songs WHERE ID IN
     (SELECT Sampled FROM SampleExportRel);
+*/
 
-/*
+CREATE OR REPLACE VIEW ExportRels AS
+  SELECT * FROM Rel LIMIT 500;
+
 CREATE OR REPLACE VIEW ExportDillaSongs AS
-  SELECT * FROM Songs WHERE ID IN (SELECT Song FROM Rel);
+  SELECT * FROM Songs WHERE ID IN (SELECT Song FROM ExportRels);
 
 CREATE OR REPLACE VIEW ExportSampledSongs AS
-  SELECT * FROM Songs WHERE ID IN (SELECT Sampled FROM Rel);
-
- */
+  SELECT * FROM Songs WHERE ID IN (SELECT Sampled FROM ExportRels);
